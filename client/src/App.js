0317fdb50f189014182eccae43fa622756b8cabd
swart.isa <swart102@umn.edu>
2018-12-02 16:05:59 -0600

    New Routes and Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import MyList from "./pages/MyList";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -11,9 +12,9 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/my-list" component={MyList} />
           <Route component={NoMatch} />
         </Switch>
       </div>
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -4,7 +4,7 @@ import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 
-class Detail extends Component {
+class Search extends Component {
   state = {
     book: {}
   };
@@ -22,25 +22,8 @@ class Detail extends Component {
         <Row>
           <Col size="md-12">
             <Jumbotron>
-              <h1>
-                {this.state.book.title} by {this.state.book.author}
-              </h1>
+              <h1>Search Page</h1>
             </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Synopsis</h1>
-              <p>
-                {this.state.book.synopsis}
-              </p>
-            </article>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/">← Back to Authors</Link>
           </Col>
         </Row>
       </Container>
@@ -48,4 +31,4 @@ class Detail extends Component {
   }
 }
 
-export default Detail;
+export default Search;
