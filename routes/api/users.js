c54017f2dfd6058f41ea72d5f88ef8358fbcba20
Cmoney <cascherer42@gmail.com>
2018-12-06 17:38:46 -0600

    on-prem authentication branch

---
+++ routes/api/users.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const usersController = require("../../controllers/usersController");
+const passport = require("passport");
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router
+    .route("/register")
+    .post(usersController.registerUser)
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router
+    .route("/login")
+    .post(usersController.loginUser);
+
+// @route GET api/users/currentuser
+// @desc Return current user
+// @access Private
+router
+    .route("/currentUser")
+    .get(
+        passport.authenticate("jwt", { session: false }),
+        usersController.currentUser
+    )
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+require("./config/passport")(passport);
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(passport.initialize());
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tvShowList");
+
+
 
 // Start the API server
 app.listen(PORT, function() {
