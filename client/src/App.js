10fda21647d24d6e49aeba7d63d450fc03b8297f
Cmoney <cascherer42@gmail.com>
2018-12-07 18:25:33 -0600

    Front end connected with backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,64 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+
+// Redux imports
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import MyList from "./pages/MyList";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/my-list" component={MyList} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+          <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/my-list" component={MyList} />
+            <Route component={NoMatch} />
+          </Switch>
+
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
