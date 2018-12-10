3671e8634dc5f78ae4743db230580739576d2607
Reeve Johnson <reevewjohnson@gmail.com>
2018-12-09 19:58:20 -0600

    Deleted imports for deleted components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,23 +3,14 @@ import API from "../utils/API";
 import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
 
 class Home extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
             <Jumbotron>
               <h1>Home</h1>
             </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
     );
   }
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
 
