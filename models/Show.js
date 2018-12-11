c9283710f8c9a82a7accedebd82ff14ae4c2deed
Cmoney <cascherer42@gmail.com>
2018-12-10 20:14:23 -0600

    updated show model

--- models/Show.js
+++ models/Show.js
@@ -14,39 +14,10 @@ const showSchema = new Schema({
       ref: "Users"
     }
   ],
-  imdbID: String,
-  title: { type: String, required: true },
-  rated: String,
-  released: Date,
-  runTime: String,
-  genre: [
-    { type: String, required: true }
-  ],
-  directors: [
-    { type: String }
-  ],
-  writers: [
-    { type: String, required: true }
-  ],
-  actors: [
-    { type: String, required: true }
-  ],
-  plot: String,
-  language: [
-    { type: String }
-  ],
-  country: [
-    { type: String }
-  ],
-  awards: [
-    { type: String }
-  ],
-  poster: String,
-  link: String,
-  date: { type: Date, default: Date.now },
-  ratings: [
-    { type: String, required: true }
-  ],
+  show: {
+    type: Object,
+    requried: true
+  },
   comments: [
     {
       type: Schema.Types.ObjectId,
