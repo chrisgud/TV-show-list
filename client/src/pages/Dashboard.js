e5ca4431f34a71f809c0570951a2d25d194e6dbe
Chris Gudmundsson <chrisgud85@gmail.com>
2018-12-15 12:23:40 -0600

    Added a card wrapper on the dashboard data

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { 
-    logoutUser, 
-    getCurrentUser, 
+import Card from '@material-ui/core/Card';
+import {
+    logoutUser,
+    getCurrentUser,
 } from "../actions/authActions";
+
+const cardStyle = {
+    width: 400,
+    margin: "auto"
+}
 
 class Dashboard extends Component {
 
@@ -20,32 +26,34 @@ class Dashboard extends Component {
     render() {
         const { user } = this.props.auth;
         return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-                <div className="row">
-                    <div className="col s12 center-align">
-                        <h4>
-                            <b>Hey there,</b> {user.name.split(" ")[0]}
-                            <b>{user.test}</b>
-                            <p className="flow-text grey-text text-darken-1">
-                                You are logged into a full-stack{" "}
-                                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-                        </h4>
-                        <button
-                            style={{
-                                width: "150px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px",
-                                marginTop: "1rem"
-                            }}
-                            onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                            Logout
-            </button>
+            <Card style={cardStyle}>
+                <div style={{ height: "75vh" }} className="container valign-wrapper">
+                    <div className="row">
+                        <div className="col s12 center-align">
+                            <h4>
+                                <b>Hey there,</b> {user.name.split(" ")[0]}
+                                <b>{user.test}</b>
+                                <p className="flow-text grey-text text-darken-1">
+                                    You are logged into a full-stack{" "}
+                                    <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                </p>
+                            </h4>
+                            <button
+                                style={{
+                                    width: "150px",
+                                    borderRadius: "3px",
+                                    letterSpacing: "1.5px",
+                                    marginTop: "1rem"
+                                }}
+                                onClick={this.onLogoutClick}
+                                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                            >
+                                Logout
+                </button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </Card>
         );
     }
 }
