2d01d721dde2e8bfeccc47111171831ce91af5d8
swart.isa <swart102@umn.edu>
2018-12-20 20:55:18 -0600

    Watch List Styles

--- client/src/components/WatchList/index.js
+++ client/src/components/WatchList/index.js
@@ -4,26 +4,25 @@ import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import { connect } from "react-redux";
-
+import './style.css'
 const styles = theme => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: 'black'
   },
   gridList: {
-    width: 400,
-    height: 500,
+    width: 500,
+    height: 700,
   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
-  },
+  }
 });
 
 function WatchListGrid(props) {
@@ -33,7 +32,7 @@ function WatchListGrid(props) {
     <div className={classes.root}>
       <GridList cellHeight={180} className={classes.gridList}>
         <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
-          <ListSubheader component="div">Watch List</ListSubheader>
+          <h1 id="title">Watch List</h1>
         </GridListTile>
         {watchList.map(show => (
           <GridListTile key={show.image}>
